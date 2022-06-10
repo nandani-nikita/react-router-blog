67af4f00998e368824ae84869f6bb6f97515e711
Nikita <nikitann2501@gmail.com>
2022-06-10 22:11:13 +0530

    Delete Post

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NewPost from './NewPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
-import { Route, Routes, useHistory, Link } from 'react-router-dom';
+import { Route, Routes, useNavigate, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
@@ -41,8 +41,12 @@ function App() {
 
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const handleDelete = async(id) => {
+    const postsList = posts.filter(post=>post.id!==id);
+    setPosts(postsList);
+    navigate('/about');
 
   }
   return (
