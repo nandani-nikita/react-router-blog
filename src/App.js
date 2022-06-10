8565dbdaaf0cf6f1daca279f1267426ee4d25d2e
Nikita <nikitann2501@gmail.com>
2022-06-10 21:23:59 +0530

    view posts

--- src/App.js
+++ src/App.js
@@ -10,19 +10,56 @@ import { Route, Routes, useHistory, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
+
+
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'abc 1',
+      date: "blah 1",
+      body: "again blah 1"
+    },
+    {
+      id: 2,
+      title: 'abc 2',
+      date: "blah 2",
+      body: "again blah 2  rgbrg rg rg brb nryy g rgnt nn rgbt nybyr ntrnbryn 4ry nry"
+    },
+    {
+      id: 3,
+      title: 'abc 3',
+      date: "blah 3",
+      body: "again blah 3"
+    },
+    {
+      id: 4,
+      title: 'abc 4',
+      date: "blah 4",
+      body: "again blah 4 fbgggg tge r brgbrygy byrgb r bgrbrg"
+    }
+  ]);
+
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleDelete = async(id) => {
+
+  }
   return (
     <div className='App'>
-       <Link to="/post">Posts</Link>
-        <Link to="/about">About</Link>
-      <Header />
-      <Nav />
+
+      <Header title={'Art Blog'} />
+      <Nav
+        search={search}
+        setSearch={setSearch}
+      />
 
       <Routes >
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Home posts={posts} />} />
 
         <Route path='post' element={<NewPost />} />
 
-        <Route path='post/:id' element={<PostPage />} />
+        <Route path='post/:id' element={<PostPage posts={posts} handleDelete={handleDelete} />} />
 
         <Route path='about' element={<About />} />
 
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,31 @@
+import { useParams, Link } from 'react-router-dom';
 import React from 'react';
 
-const PostPage = () => {
+const PostPage = ({ posts, handleDelete }) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+
   return (
-    <main>
-      <h1>Post Page</h1>
+    <main className='PostPage'>
+      <article className='post'>
+        {post &&
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.date}</p>
+            <p className='postBody'>{post.body}</p>
+            <button onClick={() => handleDelete(post.id)}>
+              Delete post
+            </button>
+          </>
+        }
+        {!post &&
+        <>
+        <h2>Page Not Found</h2>
+        <p>Well, that's disappointing</p>
+        <Link to='/'>Visit Our Homepage</Link>
+        </>
+        }
+      </article>
     </main>
   )
 }
